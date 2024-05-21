7ef4488952e4034a2890f6e75e3877d5b9961f28
Rhys Jones <rj@dusk.is>
2024-05-21 19:44:49 +1200

    More removals of junk

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,35 +7,6 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontSize: {
-      /* @link https://utopia.fyi/type/calculator?c=320,16,1.2,1240,20,1.25,10,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12 */
-    //   xs: 'clamp(0.69rem, calc(0.63rem + 0.32vw), 0.88rem)',
-    //   sm: 'clamp(0.83rem, calc(0.74rem + 0.46vw), 1.10rem)',
-    //   base: 'clamp(1.00rem, calc(0.87rem + 0.65vw), 1.38rem)',
-    //   lg: 'clamp(1.20rem, calc(1.02rem + 0.90vw), 1.72rem)',
-    //   xl: 'clamp(1.44rem, calc(1.19rem + 1.23vw), 2.15rem)',
-    //   '2xl': 'clamp(1.73rem, calc(1.40rem + 1.67vw), 2.69rem)',
-    //   '3xl': 'clamp(2.07rem, calc(1.63rem + 2.23vw), 3.36rem)',
-    //   '4xl': 'clamp(2.49rem, calc(1.89rem + 2.97vw), 4.20rem)',
-    //   '5xl': 'clamp(2.99rem, calc(2.20rem + 3.93vw), 5.25rem)',
-    //   '6xl': 'clamp(3.58rem, calc(2.55rem + 5.17vw), 6.56rem)',
-    //   '7xl': 'clamp(4.30rem, calc(2.95rem + 6.78vw), 8.20rem)',
-    //   '8xl': 'clamp(5.16rem, calc(3.39rem + 8.84vw), 10.24rem)',
-    //   '9xl': 'clamp(6.19rem, calc(3.89rem + 11.50vw), 12.81rem)',
-// https://utopia.fyi/type/calculator/?c=320,18,1.2,1280,24,1.25,10,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12
-        // xs: 'clamp(0.7813rem, 0.7217rem + 0.2979vi, 0.96rem)',
-        // sm: 'clamp(0.9375rem, 0.85rem + 0.4375vi, 1.2rem)',
-        // base: 'clamp(1.125rem, 1rem + 0.625vi, 1.5rem)',
-        // lg: 'clamp(1.35rem, 1.175rem + 0.875vi, 1.875rem)',
-        // xl: 'clamp(1.62rem, 1.3788rem + 1.2063vi, 2.3438rem)',
-        // '2xl': 'clamp(1.944rem, 1.6154rem + 1.6428vi, 2.9297rem)',
-        // '3xl': 'clamp(2.3328rem, 1.8897rem + 2.2155vi, 3.6621rem)',
-        // '4xl': 'clamp(2.7994rem, 2.2066rem + 2.9638vi, 4.5776rem)',
-        // '5xl': 'clamp(3.3592rem, 2.5716rem + 3.938vi, 5.722rem)',
-        // '6xl': 'clamp(4.0311rem, 2.9906rem + 5.2025vi, 7.1526rem)',
-        // '7xl': 'clamp(4.8373rem, 3.4695rem + 6.839vi, 8.9407rem)',
-        // '8xl': 'clamp(5.8048rem, 4.0144rem + 8.9519vi, 11.1759rem)',
-        // '9xl': 'clamp(6.9657rem, 4.631rem + 11.6736vi, 13.9698rem)',
-
         xs: 'clamp(0.7813rem, 0.735rem + 0.2313vi, 0.92rem)',
         sm: 'clamp(0.9375rem, 0.8667rem + 0.3542vi, 1.15rem)',
         base: 'clamp(1.125rem, 1.0208rem + 0.5208vi, 1.4375rem)',
@@ -122,6 +93,7 @@ module.exports = {
       },
       aspectRatio: {
         '4/3': '4 / 3',
+        '3/4': '3 / 4',
       },
       height: {
         screen: ['100vh', '100dvh'],
@@ -129,7 +101,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
     plugin(function({ addVariant }) {
         addVariant('children', '&>*')
       })
